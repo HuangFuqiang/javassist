c13253a3aae68b00a309cd74bf5a0d38ec367590
HuangFuqiang <fuqianghuang01@gmail.com>
2019-04-25 10:25:48 +0800

    [ADD]javassist-demo

---
+++ src/main/java/com/code/javassistdemo/JavassistDemoApplication.java
@@ -0,0 +1,13 @@
+package com.code.javassistdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavassistDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavassistDemoApplication.class, args);
+    }
+
+}
